53f82b3894b088e0e8d4b3199afbe483ed58d858
Alexander-krd <knjaz2503@gmail.com>
2024-11-24 22:51:54 +0300

    Update module_2_2.py

--- module_2_2.py
+++ module_2_2.py
@@ -1,9 +1,9 @@
-first = f = int(input())
-second = s = int(input())
-third= t = int(input())
-if f==s and f==t:
+first = int(input())
+second = int(input())
+third = int(input())
+if first==second and first==third:
     print(3)
-elif f==s or f==t or s==t:
+elif first==second or first==third or second==third:
     print(2)
 else:
     print(0)
