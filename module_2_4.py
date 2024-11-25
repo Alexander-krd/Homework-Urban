4c0b77483ccf07fd1f47cae0c10230b0c4b227cc
Alexander-krd <knjaz2503@gmail.com>
2024-11-25 11:13:20 +0300

    Update module_2_4.py

--- module_2_4.py
+++ module_2_4.py
@@ -1,11 +1,18 @@
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
-primes =[2, 3, 5, 7, 11, 13]
-not_primes = [4, 6, 8, 9, 10, 12, 14, 15]
-def number(num):
-    n= num
-    count = 0
-    for i in range(1,n+1):
-        if n % i == 0:
-            count += 1
-    return "True" if count ==2 else "False"
-print(number(5))
+primes =[]
+not_primes = []
+for a in numbers:
+    is_prime = True
+    if a == 1:
+        continue
+    else:
+        for b in range(2, a-1):
+            if a % b == 0:
+                is_prime = False
+                break
+    if is_prime:
+        primes.append(a)
+    else: not_primes.append(a)
+print("Список:", numbers)
+print("Простые числа:", primes)
+print("Непростые числа:", not_primes)
